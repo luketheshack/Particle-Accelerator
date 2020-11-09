ff1a9177decd8fafcea348d115b1fca35604fff6
luketheshack <lmarusha@nd.edu>
2020-11-09 16:34:52 -0500

    open file

--- project.c
+++ project.c
@@ -2,6 +2,19 @@
 // Fundamentals of Computing : Lab #11
 // project.c
 
+#include "projfunc.h"
+
 int main() {
 
+	char *fn = malloc(20);
+	fn = "elements.csv";
+	FILE *fp = fopen(fn, "r+");
+
+	
+
+	while (1) {
+		if feof(fp) break;
+	}
+
+	return 0;
 }
